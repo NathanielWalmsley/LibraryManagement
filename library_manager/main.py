aa1f66d0d657340c7daa8b46ff12a764b61d7ca0
Nathaniel Walmsley <nathaniel.walmsley@proton.me>
2022-07-25 12:52:13 +0100

    Add method and test for retrieving list of libraries

--- library_manager/main.py
+++ library_manager/main.py
@@ -10,8 +10,32 @@ class LibraryManager(object):
             self.connection = sqlite3.connect(path)
             print('Connection to SQLite Database established')
         except sqlite3.Error as e:
-            print('Unable to establish connection to SQLite database at {path}')
-            print('Error occured with the following message: {e}')
+            print(f'Unable to establish connection to SQLite database at {path}')
+            print(f'Error occured with the following message: {e}')
 
     
-    
+    def get_list_of_libraries(self):
+        """
+            Get a complete list of all the libraries managed by this
+            catalogue and return them as a string
+        """
+        query = """
+            SELECT * FROM tbl_library_branch
+        """
+        cursor = self.connection.cursor()
+        result = None
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+        except sqlite3.Error as e:
+            print(
+                f'An SQL Error for the connection to {self._connection_path} '
+                f'occurred when retrieving list of libraries.\n'
+                f'Error code is: {e}'
+            )
+        except Exception as unexpectedException:
+            print(
+                f'An unexpected error occurred with code: {unexpectedException}'
+            )
+        return result
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,35 @@
 import pytest
+import sqlite3
 from library_manager import main
 
-def test_init_establishes_connection():
-    test_path = './tests/sqlite.db'
-    catalogue = main.LibraryManager(test_path)
-    assert catalogue._connection_path == test_path
 
+TEST_PATH = './tests/sqlite.db'
+
+
+def test_init_establishes_connection():
+    catalogue = main.LibraryManager(TEST_PATH)
+    assert catalogue._connection_path == TEST_PATH
+
+
+def test_retrieve_list_of_libraries():
+    catalogue = main.LibraryManager(TEST_PATH)
+    result = catalogue.get_list_of_libraries()
+    expected = {
+        '1': {
+            'library_branch_BranchName': 'Sharpstown', 
+            'library_branch_BranchAddress': '32 Corner Road, New York, NY 10012'
+        },
+        '2': {
+            'library_branch_BranchName': 'Central', 
+            'library_branch_BranchAddress': '491 3rd Street, New York, NY 10014'
+        },
+        '3': {
+            'library_branch_BranchName': 'Saline', 
+            'library_branch_BranchAddress': '40 State Street, Saline, MI 48176'
+        },
+		'4': {
+            'library_branch_BranchName': 'Ann Arbor',
+            'library_branch_BranchAddress': '101 South University, Ann Arbor, MI 48104'
+        }
+    }
+    assert result == expected
