93ffaa0f2fd9092865bcfd9934b3fc3f2dc81eb1
Nathaniel Walmsley <nathaniel.walmsley@proton.me>
2022-08-09 16:38:22 +0100

    Add Index page for web interface

--- djangoProject/library_catalogue/apps.py
+++ djangoProject/library_catalogue/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LibraryCatalogueConfig(AppConfig):
+class djangoProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'library_catalogue'
