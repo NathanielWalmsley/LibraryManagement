499631b01599e717c17a9f9cf230499b06f1af96
Nathaniel Walmsley <nathaniel.walmsley@proton.me>
2022-08-23 15:16:17 +0100

    Migrate original schema to Django

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'library_catalogue.apps.djangoProjectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- djangoProject/library_catalogue/models.py
+++ djangoProject/library_catalogue/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class LibraryBranch(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+
+class Borrower(models.Model):
+    card_number = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    models.UniqueConstraint(
+        fields=[title, publisher],
+        name='unique_title_publisher_constraint'
+    )
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class BookAuthorLink(models.Model):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Inventory(models.Model):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    branch_id = models.ForeignKey(LibraryBranch, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    models.UniqueConstraint(
+        fields=[book_id, branch_id],
+        name='unique_book_and_branch_constraint'
+    )
+
+
+class Loan(models.Model):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    branch_id = models.ForeignKey(LibraryBranch, on_delete=models.CASCADE)
+    card_number = models.ForeignKey(Borrower, on_delete=models.CASCADE)
+    date_out = models.DateField('date loaned')
+    date_due = models.DateField('date due for return')
