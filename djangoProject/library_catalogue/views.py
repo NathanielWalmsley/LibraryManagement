4a62f7e61bc7be22161ab701a74e9a97e5af3fad
Nathaniel Walmsley <nathaniel.walmsley@proton.me>
2022-09-15 13:49:53 +0100

    Extend header settings in detail.html

--- djangoProject/library_catalogue/views.py
+++ djangoProject/library_catalogue/views.py
@@ -7,25 +7,9 @@ from .models import Book
 from .forms import SearchForm
 
 
-def search(request):
-    query = request.GET.get('searchQuery')
-    if not query:
-        return render(request, 'library_catalogue/index.html', {})
-    result = Book.objects.filter(
-        Q(title__icontains=query)
-    )
-    return render(request, 'library_catalogue/detail.html', { 'book': result })
-
-
 class HomeView(FormView):
     template_name = 'library_catalogue/home.html'
     form_class = SearchForm
-
-
-def index(request):
-    first_five_books = Book.objects.order_by('title')[:5]
-    context = {'first_five_books': first_five_books}
-    return render(request, 'library_catalogue/index.html', context)
 
 
 def detail(request, book_id):
